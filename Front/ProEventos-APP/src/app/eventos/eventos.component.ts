0edb64124732645a2e335890446944e01f7def87
FelipeCherubin <felipe_cherubin@hotmail.com>
2021-03-21 18:35:07 -0300

    add filtro de busca

--- Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -9,7 +9,28 @@ import { error } from 'selenium-webdriver';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltros: any = [];
+  widthImg: number = 50;
+  marginImg: number = 2;
+  ocultarImagem: boolean = false;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltros = this.filtroLista ? this.filtrarEventos(this.filtroLista.toLocaleLowerCase()) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  monstraImagem(): void{
+    this.ocultarImagem = !this.ocultarImagem;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltros = this.eventos
+      },
       error => console.log(error)
     );
   }
